80f45d2a4934a92dee31e854dd5bdaeef11046c3
riki900 <kleinr900@gmail.com>
2019-06-19 12:46:25 +0200

    Added new tests

--- test_calculator.py
+++ test_calculator.py
@@ -37,3 +37,49 @@ def test_add_operand2_invalid():
 
     assert str(e.value) == '"two" is not a number.'
 
+def test_subtract_operand1_invalid():
+    c = Calculator()
+    with pytest.raises(ValueError) as e:
+        assert c.subtract("one", 2)
+
+    assert str(e.value) == '"one" is not a number.'
+
+
+def test_subtract_operand2_invalid():
+    c = Calculator()
+    with pytest.raises(ValueError) as e:
+        assert c.subtract(1, "two")
+
+    assert str(e.value) == '"two" is not a number.'
+
+def test_multiply_operand1_invalid():
+    c = Calculator()
+    with pytest.raises(ValueError) as e:
+        assert c.multiply("one", 2)
+
+    assert str(e.value) == '"one" is not a number.'
+
+
+def test_multiply_operand2_invalid():
+    c = Calculator()
+    with pytest.raises(ValueError) as e:
+        assert c.multiply(1, "two")
+
+    assert str(e.value) == '"two" is not a number.'
+
+def test_divide_operand1_invalid():
+    c = Calculator()
+    with pytest.raises(ValueError) as e:
+        assert c.divide("one", 2)
+
+    assert str(e.value) == '"one" is not a number.'
+    
+def test_divide_operand2_invalid():
+    c = Calculator()
+    with pytest.raises(ValueError) as e:
+        assert c.divide(1, "two")
+
+    assert str(e.value) == '"two" is not a number.'
+
+
+
